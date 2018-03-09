bcba6e883f4f8ad649771dec92b3cede8c411e0e
K2\marko <kmarko.net@gmail.com>
2018-03-10 00:15:32 +0100

    basic ui

--- src/kmarkodotnet/src/app/app.module.ts
+++ src/kmarkodotnet/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
-
+import { MenuComponent } from './menu/menu.component';
+import { ContentComponent } from './content/content.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ContentComponent,
+    LogoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
